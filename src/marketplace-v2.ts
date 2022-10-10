32b23da2e862a1168b0586cdfb5c0f97b23997f5
Nando <24811313+fzavalia@users.noreply.github.com>
2022-10-09 22:29:40 -0300

    fix: Missing contract names

--- src/marketplace-v2.ts
+++ src/marketplace-v2.ts
@@ -1,6 +1,6 @@
-import { MetaTransactionExecuted } from "../generated/Committee/Committee";
+import { MetaTransactionExecuted } from "../generated/MarketplaceV2/MarketplaceV2";
 import { handleMetaTransactionExecuted as _handleMetaTransactionExecuted } from "./utils/handleMetaTransactionExecuted";
 
 export function handleMetaTransactionExecuted(event: MetaTransactionExecuted): void {
-  _handleMetaTransactionExecuted("Committee", event.params.userAddress, event.block.timestamp);
+  _handleMetaTransactionExecuted("MarketplaceV2", event.params.userAddress, event.block.timestamp);
 }
